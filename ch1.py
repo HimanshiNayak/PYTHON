d14cb3ad4efee0b17cb49d4ef0922708eb51544c
Himanshi Nayak <114794957+hima1502@users.noreply.github.com>
2022-11-06 12:16:43 +0530

    Add files via upload

---
+++ ch1.py
@@ -0,0 +1,40 @@
+#in python one variable can hold many values .. 
+# variables 
+from pydoc import Helper
+
+# USe ''' when we need to write many lines . and use '' or ' for single lines.
+#  if we write sometthing in " " itautomatically bcms a string , even ifit is from some other class .
+a= 3 # integer 
+b=3.5688 # float
+c= ' himanshi'
+d= " apple"
+e=''' twinkle twinlke 
+little star 
+how i wonder what you are .''' #string
+f = True # always use first capital 
+g= False # boolean
+h= None # made just for use 
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+# printing the type of variables 
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+A =10 
+print(A) # is case sensitive 
+one2 = 29 # can't use 2one or one 2  or @one 
+print(one2)
+
+
+
+
