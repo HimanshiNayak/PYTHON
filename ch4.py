f566461d19e123961f11d207b421615afb6353d4
Himanshi Nayak <114794957+hima1502@users.noreply.github.com>
2022-11-06 12:29:37 +0530

    Add files via upload

---
+++ ch4.py
@@ -0,0 +1,24 @@
+# lists and tuples .
+
+#create alist using[]
+a = [1 , 3 , 4 , 56 ]
+
+#print it using print function
+print(a)
+
+#acess index using a[0] , a[1] , etc 
+print(a[0])
+
+# change the value of the list using..
+a[0] = 98 # 98 will replace 1 in a
+print(a)
+
+# we can create a list with different types in it 
+c = [2 , 2.2 , "hello" , False ]
+print(c)
+ 
+ # list slicing :- same as the str slicing
+print(c[0:3])
+print(c[1:])
+print(c[:8])
+print(c[-1:])
