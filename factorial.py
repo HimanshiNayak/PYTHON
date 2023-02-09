90ee5c1b25ed8420db9d5a37ec241e66c0d980ce
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-02-09 23:16:00 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,26 @@
+# factorial of 5
+
+
+def factorial(n):
+    # base case: if n is 1, return 1
+    if n == 1:
+        return 1
+    # recursive case: return n multiplied by the factorial of n-1
+    else:
+        return n * factorial(n-1)
+
+# example usage
+print(factorial(5)) # prints 120
+                     
+                    # OR 
+
+# Python code to demonstrate naive method
+# to compute factorial
+n = 23
+fact = 1
+ 
+for i in range(1, n+1):
+    fact = fact * i
+ 
+print("The factorial of 23 is : ", end="")
+print(fact)
