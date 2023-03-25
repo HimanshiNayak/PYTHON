6d1cab992a5608cc60c748ff602559a0025aed57
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-03-25 23:27:40 +0530

    Update filehandeling.py

--- filehandeling.py
+++ filehandeling.py
@@ -10,3 +10,20 @@ f.close
 
 # or er can open a file as :
 # f = open("file address","r")
+
+
+# #  to read linewise linewise
+
+# # first line
+f = open("hello.txt","r")
+data = f.readline()
+print(data)
+
+# # second line
+data = f.readline()
+print(data)
+
+# # third line
+data = f.read()
+print(data)
+f.close
