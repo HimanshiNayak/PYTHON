12dfc99f9ff91e451d63f4dc3f9563b54099ea91
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2023-04-02 23:53:47 +0530

    Update practice3.py

--- oops/practice3.py
+++ oops/practice3.py
@@ -57,3 +57,42 @@ class laptop:
 laptop1 = laptop('hp',66000)
 laptop2 = laptop('dell',55000)
 print(laptop1.discount())
+
+# inheritance
+single inheritance
+
+class Parent:
+    company = "Google"
+    def id(self):
+        print("Hi guys , how are you guys ?")
+class Child(Parent):
+    company = "Youtube"
+    def name(self):
+        print("Good morning people !")
+
+p1 = Parent()
+p1.id()
+
+p2 = Child()
+p2.name()
+p2.id()
+print(p2.company)
+the object child class can use all the attributesof the parent class but vice versa is not true    #   
+if some of the att. is repeating in the child class then , it's value from the child class will be followed as , the prefferenc eof this will be more
+
+3 types of inheritance :- 
+single 
+multiple : more than one parent for a child
+multilayered : one parent one child , then comes grand child
+
+multiple inheritance
+
+class Employee:
+    company = "Visa"
+    ecode = 120
+
+class Freelancer:
+    company = "Fiverr"
+    level = 0
+    def upgradeLevel(self):
+        self.level = self.level + 1
